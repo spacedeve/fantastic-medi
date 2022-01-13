401a583aeb61e327536bb56b44f8039cb8c9b743
Ozod Abdusamatov <oabdusamatov@gmail.com>
2022-01-13 10:16:04 +0500

    Finished Slider!
    * Fixed bug A

--- js/script.js
+++ js/script.js
@@ -16,3 +16,37 @@ window.onclick = function(event) {
   }
 }
 
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.hero__btn--right');
+const prevBtn = document.querySelector('.hero__btn--left');
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener('click', function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+if(counter === slides.length){
+  counter = 0;
+}
+if(counter < 0 ){
+  counter = slides.length - 1;
+}
+
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%`;
+  });
+}
